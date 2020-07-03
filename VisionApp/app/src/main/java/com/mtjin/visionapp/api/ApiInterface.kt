d27fee34691ca158f66ef1cd72515ddbcf14260b
jackjackE <tmddjs210@naver.com>
2020-07-03 17:11:16 +0900

    이미지 업로드 및 좌표값 서버에 전달 구현완료

--- VisionApp/app/src/main/java/com/mtjin/visionapp/api/ApiInterface.kt
+++ VisionApp/app/src/main/java/com/mtjin/visionapp/api/ApiInterface.kt
@@ -1,18 +1,20 @@
 package com.mtjin.visionapp.api
 
-import com.mtjin.visionapp.model.Point
 import okhttp3.MultipartBody
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.PartMap
 
 interface ApiInterface {
     @Multipart
     @POST("/predict")
     fun getTest(
         @Part file: MultipartBody.Part,
-        @Part("y") x: Float,
-        @Part("x") y:  Float
+        @Part("x") x: ArrayList<Float>,
+        @Part("y") y: ArrayList<Float>
     ): Call<ResponseBody>
 
 //    @FormUrlEncoded
