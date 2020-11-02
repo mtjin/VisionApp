0619f8099db7f5cd4825b8a0aa7b996a2ddfa8e5
parkwonro <park-7163@hanmail.net>
2020-11-03 02:32:17 +0900

    point 생성 및 마스크 생성 수정

--- backgroundBlur.py
+++ backgroundBlur.py
@@ -7,7 +7,7 @@ def target_remove(img, mask):
     print(img.shape)
     for i in range(len(img)):
         for j in range(len(img[0])):
-            if mask[i][j]> 220:
+            if mask[i][j]> 200:
                target_img[i, j, :] = img[i, j, :]
                remove_img[i,j,:] = 0
                mask[i][j] = 255
@@ -18,23 +18,8 @@ def target_remove(img, mask):
 def combination(background, target, mask):
     for i in range(len(mask)):
         for j in range(len(mask[0])):
-            if mask[i][j]> 220:
+            if mask[i][j]> 200:
                background[i,j,:] = target[i,j,:]
     return background
-# def back_ground(remove_img, mask):
-#     neighbor=[[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-#
-#     for i in range(len(remove_img)):
-#         for j in range(len(remove_img[0])):
-#             if mask[i][j] > 100:
-#                 for p in range(len(remove_img[0][0])):
-#                     hist = []
-#                     for k in range(len(neighbor)):
-#                         a = neighbor[k][0]
-#                         b = neighbor[k][1]
-#                         if sum(remove_img[i+a,i+b,:]) > 0:
-#                             hist.append(remove_img[i+a,i+b,p])
-#                     remove_img[i,j,p] = np.mean(hist)
-#     back_img = remove_img
-#     return back_img
+
 
--- model/COCO_Data_Unet_model.py
+++ model/COCO_Data_Unet_model.py
@@ -3,20 +3,91 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, concatenate
 from keras.models import load_model
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import to_categorical
 import random
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+class DataGenerator(Sequence):
+    def __init__(self, p,b,o, y, batch_size, dim, dimy, n_channels, shuffle=False):
+        self.X = p
+        self.o = o
+        self.b = b
+        self.y = y if y is not None else y
+        self.batch_size = batch_size
+        self.dim = dim
+        self.dimy = dimy
+        self.n_channels = n_channels
 
-from tensorflow.python.keras.optimizers import SGD
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.X))
+        if self.shuffle:
+            np.random.shuffle(self.indexes)
+
+    def __len__(self):
+        return int(np.floor(len(self.X) / self.batch_size))
+
+    def __data_generation(self, X_list,b_list,o_list, y_list):
+        X = np.empty((self.batch_size, *self.dim))
+        y = np.empty((self.batch_size,*self.dimy))
+        temp = np.empty((self.batch_size,288,480))
+        if y is not None:
+            # 지금 같은 경우는 MNIST를 로드해서 사용하기 때문에
+            # 배열에 그냥 넣어주면 되는 식이지만,
+            # custom image data를 사용하는 경우
+            # 이 부분에서 이미지를 batch_size만큼 불러오게 하면 됩니다.
+            for index, (pointpath,bpath,opath, labelpath) in enumerate(zip(X_list,b_list,o_list, y_list)):
+                # print(index)
+                # print(pointpath)
+                # print(bpath)
+                # print(opath)
+                # print(labelpath)
+                mask = cv2.imread(labelpath, cv2.IMREAD_GRAYSCALE)
+                dstmask = cv2.resize(mask, dsize=(480, 288), interpolation=cv2.INTER_AREA)
+                img = cv2.imread(opath, cv2.IMREAD_COLOR)
+                dstimg = cv2.resize(img, dsize=(480, 288), interpolation=cv2.INTER_AREA)
+                Point = cv2.imread(pointpath, cv2.IMREAD_GRAYSCALE)
+                dstPoint = cv2.resize(Point, dsize=(480, 288), interpolation=cv2.INTER_AREA)
+                Back = cv2.imread(bpath, cv2.IMREAD_GRAYSCALE)
+                dstBack = cv2.resize(Back, dsize=(480, 288), interpolation=cv2.INTER_AREA)
+                tmask = np.array(dstmask)
+                timg = np.array(dstimg)
+                train_Point = np.array(dstPoint)
+                train_Back = np.array(dstBack)
+                X[index, :, :, 0:3] = timg
+                X[index, :, :, 3] = train_Point
+                X[index, :, :, 4] = train_Back
+                X = X.reshape(len(X), 288, 480, 5).astype('float32') / 255.0
+                temp[index, :, :] = tmask
+                y = temp.reshape(len(temp), 288, 480, 1).astype('float32') / 255.0
+            return X, y
 
 
+    def __getitem__(self, index):
+        indexes = self.indexes[index * self.batch_size: (index + 1) * self.batch_size]
+        X_list = [self.X[k] for k in indexes]
+        b_list = [self.b[k] for k in indexes]
+        o_list = [self.o[k] for k in indexes]
+        if self.y is not None:
+            y_list = [self.y[k] for k in indexes]
+            X, y = self.__data_generation(X_list,b_list,o_list, y_list)
+            return X, y
+        else:
+            y_list = None
+            X = self.__data_generation(X_list,b_list,o_list, y_list)
+            return X
 def UNet():
-    input_size = (288, 480, 4)
+    input_size = (288, 480, 5)
     inp = Input(input_size)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inp)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
@@ -67,14 +138,22 @@ def UNet():
 
 if __name__ == "__main__":
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]="6"
-    pointdir = glob.glob('/tmp/pycharm_project_368/Person_Point2/*')
-    # backfiles = glob.glob('/tmp/pycharm_project_368/backgrounds71/*')
-    maskfiles = glob.glob('/tmp/pycharm_project_368/Personmask2/*')
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+    pointdir = glob.glob('/home/su_j615/out_focusing/trainpoints10/*')
+    backfiles = glob.glob('/home/su_j615/out_focusing/trainbackpoints10/*')
+    maskfiles = glob.glob('/home/su_j615/out_focusing/trainmasks/*')
+    valpointdir = glob.glob('/home/su_j615/out_focusing/valpoints10/*')
     length = int(len(pointdir))
     print(length)
 
-    #Train
+    # Parameters
+    params = {'dim': (288,480, 5),
+              'dimy': (288,480, 1),
+              'batch_size': 16,
+              'n_channels': 1,
+              'shuffle': False}
+
+    # Train
     trainMask = []
     trainImg = []
     trainPoint = []
@@ -86,110 +165,79 @@ if __name__ == "__main__":
     validPoint = []
     validBack = []
 
-    m = int(length * 8 / 10)
-    for j in range(m):
-        pointfiles = glob.glob(pointdir[j]+'/*')
-        random.shuffle(pointfiles)
-        for i in range(int(len(pointfiles))):
-            trainPoint.append(pointfiles[i])
-            base = os.path.basename(pointfiles[i])
-            filename=os.path.splitext(base)[0]
-            Imgpath = '/tmp/pycharm_project_368/val2017/' + filename[0:12] +'.jpg'
-            if(not os.path.isfile(Imgpath)):
-                continue
-            trainImg.append(Imgpath)
-            maskpath = '/tmp/pycharm_project_368/Personmask2/'+base
-            trainMask.append(maskpath)
-            # backpath = pointfiles[i].replace('/targets71', '/backgrounds71')
-            # trainBack.append(backpath)
-            print(pointfiles[i])
-            print(Imgpath)
-            print(maskpath)
-            # print(backpath)
+    for catindex in range(79):
 
-    print("valid")
+        print(pointdir[catindex])
+        catpointdir = glob.glob(pointdir[catindex]+'/*')
+        catpointdir = sorted(catpointdir)
+        for j in range(int(len(catpointdir))):
+            pointfiles = glob.glob(catpointdir[j]+'/*')
+            l = int(len(pointfiles))
+            random.shuffle(pointfiles)
+            mid = int(l)
+            end = l
+            for i in range(mid):
 
-    for j in range(m, length):
-        pointfiles = glob.glob(pointdir[j] + '/*')
-        random.shuffle(pointfiles)
-        for i in range(int(len(pointfiles))):
-            validPoint.append(pointfiles[i])
-            base = os.path.basename(pointfiles[i])
-            filename = os.path.splitext(base)[0]
-            Imgpath = '/tmp/pycharm_project_368/val2017/' + filename[0:12] + '.jpg'
-            validImg.append(Imgpath)
-            maskpath = '/tmp/pycharm_project_368/Personmask2/'+base
-            validMask.append(maskpath)
-            # backpath = pointfiles[i].replace('/targets71', '/backgrounds71')
-            # validBack.append(backpath)
-            print(pointfiles[i])
-            print(Imgpath)
-            print(maskpath)
-            # print(backpath)
+                base = os.path.basename(pointfiles[i])
+                filename=os.path.splitext(base)[0]
+                Imgpath = '/home/su_j615/out_focusing/train2017/' + filename[0:12] +'.jpg'
+                maskpath = pointfiles[i].replace('/trainpoints10', '/trainmasks')
+                pointindex = maskpath.split('/')[6]
+                maskpath = maskpath.replace('/' + pointindex, '')
+                backpath = pointfiles[i].replace('/trainpoints10', '/trainbackpoints10')
+                if (not os.path.isfile(Imgpath) or not os.path.isfile(maskpath) or not os.path.isfile(backpath)):
+                    continue
+                trainPoint.append(pointfiles[i])
+                trainImg.append(Imgpath)
 
+                # print(pointindex )
 
-    train_x = np.zeros((len(trainImg), 288, 480, 4))
-    valid_x = np.zeros((len(validImg), 288, 480, 4))
-    # train_x = np.zeros((len(trainImg),288,480, 5))
-    # valid_x = np.zeros((len(validImg),288,480, 5))
-    train_mask_y = np.zeros((len(trainMask),288,480))
-    valid_mask_y = np.zeros((len(validMask),288,480))
+                # print(maskpath)
+                trainMask.append(maskpath)
 
-    #입력(이미지,Point정보) 288*480 형태로 resize 후 4차원으로 만들기
-    #train
-    for index in range(len(trainImg)):
-        try:
-            mask = cv2.imread(trainMask[index], cv2.IMREAD_GRAYSCALE)
-            dstmask = cv2.resize(mask, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-            img = cv2.imread(trainImg[index], cv2.IMREAD_COLOR)
-            dstimg = cv2.resize(img, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-            Point = cv2.imread(trainPoint[index], cv2.IMREAD_GRAYSCALE)
-            dstPoint = cv2.resize(Point, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-            # Back = cv2.imread(trainBack[index], cv2.IMREAD_GRAYSCALE)
-            # dstBack = cv2.resize(Back, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-            tmask = np.array(dstmask)
-            timg = np.array(dstimg)
-            train_Point = np.array(dstPoint)
-            # train_Back = np.array(dstBack)
-            train_x[index,:,:,0:3] = timg
-            train_x[index,:,:,3] = train_Point
-            # train_x[index, :, :, 4] = train_Back
-            train_mask_y[index,:,:] = tmask
-        except Exception as e:
-            print(str(e))
+                trainBack.append(backpath)
+                # print(Imgpath)
+                # print(maskpath)
+                # print(pointfiles[i])
+                # print(backpath)
+        catpointdir = glob.glob(valpointdir[catindex] + '/*')
+        catpointdir = sorted(catpointdir)
+        for j in range(int(len(catpointdir))):
+            valpointfiles = glob.glob(catpointdir[j] + '/*')
+            for k in range(int(len(valpointfiles))):
 
-    #valid
-    for index in range(len(validImg)):
-        mask = cv2.imread(validMask[index], cv2.IMREAD_GRAYSCALE)
-        dstmask = cv2.resize(mask, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-        img = cv2.imread(validImg[index], cv2.IMREAD_COLOR)
-        dstimg = cv2.resize(img, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-        Point = cv2.imread(validPoint[index], cv2.IMREAD_GRAYSCALE)
-        dstPoint = cv2.resize(Point, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-        # Back = cv2.imread(validBack[index], cv2.IMREAD_GRAYSCALE)
-        # dstBack = cv2.resize(Back, dsize=(480, 288), interpolation=cv2.INTER_AREA)
-        vmask = np.array(dstmask)
-        vimg = np.array(dstimg)
-        valid_Point =np.array(dstPoint)
-        # valid_Back = np.array(dstBack)
-        valid_x[index, :, :, 0:3] = vimg
-        valid_x[index, :, :, 3] = valid_Point
-        # valid_x[index, :, :, 4] = valid_Back
-        valid_mask_y[index,:,:] = vmask
+                base = os.path.basename(valpointfiles[k])
+                filename = os.path.splitext(base)[0]
+                Imgpath = '/home/su_j615/out_focusing/val2017/' + filename[0:12] +'.jpg'
+                maskpath = valpointfiles[k].replace('points10', 'masks')
+                valpointindex = maskpath.split('/')[6]
+                maskpath = maskpath.replace('/' + valpointindex, '')
+                backpath = valpointfiles[k].replace('points10', 'backpoints10')
+                if (not os.path.isfile(Imgpath) or not os.path.isfile(maskpath) or not os.path.isfile(backpath)):
+                    continue
+                validPoint.append(valpointfiles[k])
+                validImg.append(Imgpath)
+                validMask.append(maskpath)
+                validBack.append(backpath)
+                # print(Imgpath)
+                # print(maskpath)
+                # print(valpointfiles[k])
+                # print(backpath)
 
-    train_x = train_x.reshape(len(train_x), 288, 480, 4).astype('float32')/255.0
-    valid_x = valid_x.reshape(len(valid_x), 288, 480, 4).astype('float32')/255.0
-    train_mask_y = train_mask_y.reshape(len(train_mask_y), 288, 480, 1).astype('float32')/255.0
-    valid_mask_y = valid_mask_y.reshape(len(valid_mask_y), 288, 480, 1).astype('float32')/255.0
+    print(len(trainImg))
+    print(len(validImg))
 
     print('start')
+    training_generator = DataGenerator(trainPoint,trainBack,trainImg, trainMask, **params)
+    validation_generator = DataGenerator(validPoint,validBack,validImg, validMask, **params)
+    earlystopping = EarlyStopping(monitor='val_loss',  # 모니터 기준 설정 (val loss)
+                                  patience=10,  # 10회 Epoch동안 개선되지 않는다면 종료
+                                  )
+    model = UNet()
+    model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+    model.summary()
+    results = model.fit(training_generator, validation_data=(validation_generator), epochs=150)
+    modelname = "point125_10_v1.h5"
+    model.save(modelname)
 
-    # model = UNet()
-    # model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-    # model.summary()
-    model = load_model('COCOv4.h5')
-    from keras.callbacks import EarlyStopping
-    early_stopping = EarlyStopping(patience=5)
-    results = model.fit(train_x, train_mask_y, validation_data=(valid_x, valid_mask_y), epochs=15, batch_size=16, verbose=1, callbacks=[early_stopping])
-    model.save("COCOv5.h5")
     print('finish')
