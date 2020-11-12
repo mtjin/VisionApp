8fe129e17a0a702f29ca85de3aa3aaf2579f2493
parkwonro <park-7163@hanmail.net>
2020-11-13 01:03:58 +0900

    backgroundcreatePoint 코드 수정

--- COCO_dataset_preprocessing/backgroundcreatePoint.py
+++ COCO_dataset_preprocessing/backgroundcreatePoint.py
@@ -23,22 +23,22 @@ def createPointLabel(TPL, image):
     x = len(image[0])
     patch = np.zeros((y,x))
     # pointpatch = pointPatch
-    temp = np.full(( 21 , 21 ), 255 )
+    temp = np.full(( 65 , 65 ), 255 )
     if(len(TPL)>0):
         index = randint(0,len(TPL)-1)
-        kernel1d = cv2.getGaussianKernel(21, 5)
+        kernel1d = cv2.getGaussianKernel(65, 7)
         kernel2d = np.outer(kernel1d, kernel1d.transpose())
 
         gaussianPath = temp * kernel2d
-        k = 255 / gaussianPath[10][10]
+        k = 255 / gaussianPath[32][32]
         gaussianPath = k*gaussianPath
         patchY = TPL[index][0]
         patchX = TPL[index][1]
-        for j in range(patchY-10, patchY+11):
-            for k in range(patchX-10, patchX+11):
+        for j in range(patchY-32, patchY+33):
+            for k in range(patchX-32, patchX+33):
                 if(j<0 or k<0 or j > y-1 or k>x-1):
                     continue
-                patch[j][k] = gaussianPath[j-(patchY-10)][k-(patchX-10)]
+                patch[j][k] = gaussianPath[j-(patchY-32)][k-(patchX-32)]
     return patch
 
 
@@ -48,24 +48,24 @@ if __name__ == "__main__":
     maskdirNames = '/home/su_j615/out_focusing/trainmasks'
     # maskdirNames = '/home/su_j615/out_focusing/valmasks'
     dirName = glob.glob(maskdirNames + "/*")
-    k=0
-    for j in range(len(dirName)):
-        fileNames = glob.glob(dirName[j] + "/*")
-        print(len(fileNames))
-        for i in range(len(fileNames)):
-            # print(str(i))
-            image = cv2.imread(fileNames[i], cv2.IMREAD_GRAYSCALE)
-            print(fileNames[i])
-            TPL = targetPixelList(image)
-            patch = createPointLabel(TPL, image)
-            dirpath = dirName[j].replace('trainmasks', 'trainbackpoints') + '/point' + str(k)
-            # dirpath = dirName[j].replace('valmasks', 'valbackpoints') + '/point' + str(k)
-            print(dirpath)
-            if not (os.path.isdir(dirpath)):
-                os.makedirs(os.path.join(dirpath))
-            path = fileNames[i][fileNames[i].rfind('/') + 1:]
-            path = dirpath + '/' + path
-            print(path)
-            cv2.imwrite(path, patch)
+    for k in range(4):
+        for j in range(len(dirName)):
+            fileNames = glob.glob(dirName[j] + "/*")
+            print(len(fileNames))
+            for i in range(len(fileNames)):
+                # print(str(i))
+                image = cv2.imread(fileNames[i], cv2.IMREAD_GRAYSCALE)
+                print(fileNames[i])
+                TPL = targetPixelList(image)
+                patch = createPointLabel(TPL, image)
+                dirpath = dirName[j].replace('trainmasks', 'trainbackpoints') + '/point' + str(k)
+                # dirpath = dirName[j].replace('valmasks', 'valbackpoints') + '/point' + str(k)
+                print(dirpath)
+                if not (os.path.isdir(dirpath)):
+                    os.makedirs(os.path.join(dirpath))
+                path = fileNames[i][fileNames[i].rfind('/') + 1:]
+                path = dirpath + '/' + path
+                print(path)
+                cv2.imwrite(path, patch)
 
 
