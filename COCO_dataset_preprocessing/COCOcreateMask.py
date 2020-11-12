2ad30ac17067cf9630918c5f603a85928863859e
parkwonro <park-7163@hanmail.net>
2020-11-13 00:57:05 +0900

    train mask 생성 및 reshape 코드 수정

--- COCO_dataset_preprocessing/COCOcreateMask.py
+++ COCO_dataset_preprocessing/COCOcreateMask.py
@@ -7,15 +7,28 @@ import pylab
 import cv2
 import os
 
+def change(image):
+    x = len(image[0])
+    y = len(image)
+    pointlist = []
+    for i in range(y):
+        for j in range(x):
+            if(100 > image[i][j]):
+                image[i][j] = 0
+            else:
+                image[i][j] = 255
+    return image
+
+
 if __name__ == "__main__":
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_ 0"] = "4"
     pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
-    dataDir='/tmp/pycharm_project_368'
-    dataType='val2017'
+    dataDir='/home/su_j615/out_focusing'
+    dataType='train2017'
     annFile='{}/annotations/instances_{}.json'.format(dataDir,dataType)
-    maskDir='/tmp/pycharm_project_368/Personmask2/'
+    maskDir='/home/su_j615/out_focusing/trainmasks/'
     coco=COCO(annFile)
 
 
@@ -24,20 +37,36 @@ if __name__ == "__main__":
     nms=[cat['name'] for cat in cats]
     print(len(nms))
 
-    for i in range(1,len(nms)):
+    for i in range(len(nms)):
         catIds = coco.getCatIds(catNms=[nms[i]])
-        imgIds = coco.getImgIds(catIds=catIds);
-        print(len(imgIds))
-        for j in range(5):
-            print(j)
+        imgIds = coco.getImgIds(catIds=catIds)
+
+
+        dirpath = maskDir + nms[i] + '/'
+        print(dirpath)
+        if not (os.path.isdir(dirpath)):
+            os.makedirs(os.path.join(dirpath))
+        imglen = len(imgIds)
+        if(imglen > 300):
+            imglen = 300
+        for j in range(2,imglen):
             img = coco.loadImgs(imgIds[j])[0]
             annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
             anns = coco.loadAnns(annIds)
+
             mask = coco.annToMask(anns[0])
             for k in range(len(anns)):
-                maskfile_path = maskDir + img['file_name']
+                maskfile_path = dirpath + img['file_name']
                 maskfile_path = maskfile_path.replace(".jpg", "")
                 maskfile_path = maskfile_path + str(k) + '.png'
-                mask = coco.annToMask(anns[k])
-                matplotlib.image.imsave(maskfile_path, mask)
+                if not (os.path.isfile(maskfile_path)):
+                    mask = coco.annToMask(anns[k])
+                    matplotlib.image.imsave(maskfile_path, mask)
+                    chimg = cv2.imread(maskfile_path, cv2.IMREAD_GRAYSCALE)
+                    changeimg = change(chimg)
+                    cv2.imwrite(maskfile_path, changeimg)
+                    chimg = cv2.imread(maskfile_path, cv2.IMREAD_GRAYSCALE)
+                    reshapeimg = cv2.resize(chimg, dsize=(854, 480), interpolation=cv2.INTER_AREA)
+                    cv2.imwrite(maskfile_path, reshapeimg)
+
 
